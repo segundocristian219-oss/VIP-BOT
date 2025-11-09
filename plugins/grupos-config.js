c0b597adf3749c58eca3f3c5b4386a2103632c22
AngelBotsD <angelperez22448@gmail.com>
2025-11-09 16:47:00 -0600

    Crear grupos-config.js

---
+++ plugins/grupos-config.js
@@ -0,0 +1,31 @@
+let handler = async (m, { conn }) => {
+const lower = m.text.toLowerCase()
+
+let isClose = {
+abrir: "not_announcement",
+cerrar: "announcement",
+"grupo abrir": "not_announcement",
+"grupo cerrar": "announcement",
+open: "not_announcement",
+close: "announcement",
+".abrir": "not_announcement",
+".cerrar": "announcement",
+".grupo abrir": "not_announcement",
+".grupo cerrar": "announcement",
+".open": "not_announcement",
+".close": "announcement",
+".grupo open": "not_announcement",
+".grupo close": "announcement"
+}[lower]
+
+if (!isClose) return
+
+await conn.groupSettingUpdate(m.chat, isClose)
+await conn.sendMessage(m.chat, { react: { text: '✅', key: m.key } })
+}
+
+handler.customPrefix = /^(?:\.?grupo\s(?:abrir|cerrar|open|close)|\.?(?:abrir|cerrar|open|close))$/i;
+handler.command = new RegExp();
+handler.group = true;
+handler.group = true;
+export default handler;
