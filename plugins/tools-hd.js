df6502a647b22125db413630084b7a4aa48427c7
AngelBotsD <angelperez22448@gmail.com>
2025-11-23 11:00:29 -0600

    Actualizar tools-hd.js

--- plugins/tools-hd.js
+++ plugins/tools-hd.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
   try {
     await conn.sendMessage(m.chat, { react: { text: '⚡', key: m.key } })
 
-  conn.reply(m.chat, `${emoji} Mejorando la calidad de la imagen....`, m, fake)  
+  conn.reply(m.chat, `${emoji} Mejorando la calidad de la imagen....`, m,)  
     const media = await quoted.download()
     const ext = mime.split('/')[1]
     const filename = `mejorada_${Date.now()}.${ext}`
