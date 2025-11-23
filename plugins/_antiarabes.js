878e4a868431692330b5dc5e7807fc6e749a544e
AngelBotsD <angelperez22448@gmail.com>
2025-11-23 16:30:21 -0600

    Actualizar _antiarabes.js

--- plugins/_antiarabes.js
+++ plugins/_antiarabes.js
@@ -1,3 +1,5 @@
+// plugins/antiarabes_participants.js
+
 const arabes = [
   "20","212","213","216","218","222","224","230","234","235","237","238","249",
   "250","251","252","253","254","255","257","258","260","263","269","960","961",
@@ -9,25 +11,25 @@ export async function participantsUpdate({ id, participants, action }, { conn })
   if (action !== "add") return;
 
   const chat = global.db.data.chats[id] || {};
-
-  if (!chat.antiArabes) return;
+  if (!chat.antiArabes) return; // si está desactivado → no hace nada
 
   for (let user of participants) {
     const number = user.split("@")[0];
 
-    // Ahora sí detecta el prefijo REAL
-    const prefix = number.slice(0, 3);
+    // toma prefijo (códigos de país)
+    const prefix = number.slice(0, number.length - 7);
 
     if (arabes.includes(prefix)) {
       try {
         await conn.sendMessage(id, {
-          text: `⚠️ El número *+${number}* no está permitido en este grupo.`,
+          text: `🚫 El número *+${number}* no está permitido en este grupo.`,
           mentions: [user]
         });
 
         await conn.groupParticipantsUpdate(id, [user], "remove");
+
       } catch (err) {
-        console.error("Error al expulsar número bloqueado:", err);
+        console.error("Error expulsando árabe:", err);
       }
     }
   }
