7d3397e7507f5977858e221cf353bc306eee9582
AngelBotsD <angelperez22448@gmail.com>
2025-11-29 01:34:23 -0600

    Actualizar grupos-link.js

--- plugins/grupos-link.js
+++ plugins/grupos-link.js
@@ -1,11 +1,10 @@
 var handler = async (m, { conn, args }) => {
-let group = m.chat
-const pp = await conn.profilePictureUrl(group, 'image').catch((_) => 'https://files.catbox.moe/xr2m6u.jpg')
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-let message = `\t*⌁☍꒷₊˚ Link del Grupo ꒷₊˚⌁☍*\n\n> \`Link:\` ${link}`
-await conn.sendMessage(group, { image: { url: pp }, caption: message })
-}
 
+let group = m.chat
+let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
+conn.reply(m.chat, '\t\t☆ Aquí está el link del grupo.\n\n\v' + link, m, { detectLink: true })
+
+}
 handler.help = ['link']
 handler.tags = ['grupo']
 handler.command = ['link', 'enlace']
